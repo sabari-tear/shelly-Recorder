80f59a0f9580a369abcbd01450ce5f3edde93d8d
Sabari3000 <sabarinathan30012005@gmail.com>
2025-05-16 11:27:43 +0530

    made robust by new features

--- mainwindow.h
+++ mainwindow.h
@@ -10,11 +10,17 @@
 #include <QVariant>
 #include <QVariantList>
 #include <QDebug>
+#include <QTimer>
 
 #include "screenrecorder.h"
 
 class MainWindow : public QObject {
     Q_OBJECT
+    Q_PROPERTY(bool isRecording READ isRecording NOTIFY recordingStateChanged)
+    Q_PROPERTY(bool isPaused READ isPaused NOTIFY pauseStateChanged)
+    Q_PROPERTY(int recordingDuration READ recordingDuration NOTIFY durationChanged)
+    Q_PROPERTY(QString lastError READ lastError NOTIFY errorOccurred)
+
 public:
     explicit MainWindow(QObject* parent = nullptr);
     ~MainWindow();
@@ -39,6 +45,21 @@ public:
 
     Q_INVOKABLE void get_fps(int fps);
     Q_INVOKABLE void get_quality(int quality);
+    Q_INVOKABLE void cancelRecording();
+
+    bool isRecording() const { return m_isRecording; }
+    bool isPaused() const { return m_isPaused; }
+    int recordingDuration() const { return m_recordingDuration; }
+    QString lastError() const { return m_lastError; }
+
+signals:
+    void recordingStateChanged(bool isRecording);
+    void pauseStateChanged(bool isPaused);
+    void durationChanged(int duration);
+    void errorOccurred(const QString &error);
+
+private slots:
+    void updateRecordingDuration();
 
 private:
     //details
@@ -51,6 +72,12 @@ private:
 
     mutex m;
     condition_variable cv;
+
+    bool m_isRecording = false;
+    bool m_isPaused = false;
+    int m_recordingDuration = 0;
+    QString m_lastError;
+    QTimer m_durationTimer;
 };
 
 #endif // MAINWINDOW_H
